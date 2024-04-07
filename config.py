d84a5c4831b4a456506621f9051b4523069d3a78
ohmyqueenmedusa <165349948+ohmyqueenmedusa@users.noreply.github.com>
2024-04-07 20:56:11 +0630

    Update config.py
    Signed-off-by: ohmyqueenmedusa <165349948+ohmyqueenmedusa@users.noreply.github.com>

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("")
+MONGO_DB_URI = getenv("mongodb+srv://ohmyqueenmedusa:ohmyqueenmedusasecond@ohmyqueenmedusa.zroc51l.mongodb.net/?retryWrites=true&w=majority")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1200))
 
